2e6c5a19ad5c3506d37929f858240867afc95b26
James Jia <32031865+wenzhaojia2000@users.noreply.github.com>
2024-03-18 18:00:35 +0000

    Grouped some b64 tests to be parameterised tests

--- tests/test.b64.js
+++ tests/test.b64.js
@@ -1,23 +1,28 @@
 describe("b64.js", function () {
-	describe('when "0" (dec) is converted to b64', function () {
-		it('returns "A"', function () {
-			const result = b64.to("0");
-			expect(result).to.equal("A");
-		});
+	describe('when dec is converted to b64', function () {
+		function test_dec_to_b64(val, expected) {
+			return () => {
+				const result = b64.to(val);
+				expect(result).to.equal(expected);
+			}
+		}
+
+		it('translates "0" correctly', test_dec_to_b64("0", "A"));
+		it('translates medium-sized number correctly', test_dec_to_b64("262143", "___"));
+		it('translates large-sized number correctly', test_dec_to_b64("1237628137033103787049565361", "_-9876543210zyx"));
 	});
 
-	describe('when 262143 (dec) is converted to b64', function () {
-		it('returns "___"', function () {
-			const result = b64.to("262143");
-			expect(result).to.equal("___");
-		});
-	});
+	describe('when b64 is converted to dec', function () {
+		function test_b64_to_dec(val, expected) {
+			return () => {
+				const result = b64.from(val);
+				expect(result).to.equal(expected);
+			}
+		}
 
-	describe('when large number is converted to b64', function () {
-		it('returns correct result', function () {
-			const result = b64.to("1237628137033103787049565361");
-			expect(result).to.equal("_-9876543210zyx");
-		});
+		it('translates "A" correctly', test_b64_to_dec("A", "0"));
+		it('translates medium-sized number correctly', test_b64_to_dec("ZZZ", "104025"));
+		it('translates large-sized number correctly', test_b64_to_dec("KQXbiTLPjpWIdls1xyBX4dxC8l--oa4Ih6Saqdwonae7y", "304008007000907005070000600028035076960000000005000004003000000546000000890040050"));
 	});
 
 	describe('when string padded with 0s is converted to b64', function () {
@@ -27,38 +32,17 @@ describe("b64.js", function () {
 		});
 	});
 
-	describe('when number instead of string is converted to b64', function () {
-		it('interprets correctly (up to MAX_SAFE_INTEGER)', function () {
-			const result = b64.to(9007199254740990);
-			expect(result).to.equal("f_______-");
-		});
-	});
-
-	describe('when "A" (b64) is converted to dec', function () {
-		it('returns "0"', function () {
-			const result = b64.from("A");
-			expect(result).to.equal("0");
-		});
-	});
-
-	describe('when "ZZZ" (b64) is converted to dec', function () {
-		it('returns "104025"', function () {
-			const result = b64.from("ZZZ");
-			expect(result).to.equal("104025");
-		});
-	});
-	
-	describe('when large number is converted to dec', function () {
-		it('returns correct result', function () {
-			const result = b64.from("KQXbiTLPjpWIdls1xyBX4dxC8l--oa4Ih6Saqdwonae7y");
-			expect(result).to.equal("304008007000907005070000600028035076960000000005000004003000000546000000890040050");
-		});
-	});
-
 	describe('when string padded with As is converted to dec', function () {
 		it('interprets correctly', function () {
 			const result = b64.from("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAt");
 			expect(result).to.equal("45");
+		});
+	});
+
+	describe('when number instead of string is converted to b64', function () {
+		it('interprets correctly (up to MAX_SAFE_INTEGER)', function () {
+			const result = b64.to(9007199254740990);
+			expect(result).to.equal("f_______-");
 		});
 	});
 });
