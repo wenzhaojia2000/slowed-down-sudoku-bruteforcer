f78e6db7a090160710317402d62aeb6fd5198a36
James Jia <32031865+wenzhaojia2000@users.noreply.github.com>
2024-04-10 21:52:10 +0100

    Add some negative tests for bruteforcer.js.

--- tests/test.bruteforcer.js
+++ tests/test.bruteforcer.js
@@ -54,4 +54,119 @@ describe("bruteforcer.js", function () {
 		it('solves sudoku correctly (column)', test_solution(bf_column));
 		it('solves sudoku correctly (efficient)', test_solution(bf_efficient));
 	});
+
+	describe('when invalid matrix is given to bruteforcer', function () {
+		function test_invalid_matrix_throws(matrix) {
+			return () => {
+				const result = () => {return new Bruteforcer(matrix)};
+				expect(result).to.throw(InvalidSudokuError);
+			}
+		}
+		const invalid_type = 4;
+		const invalid_object = {
+			0: {a: 5, b: 3, c: 0, d: 0, e: 7, f: 0, g: 0, h: 0, i: 0},
+			1: {a: 6, b: 0, c: 0, d: 1, e: 9, f: 5, g: 0, h: 0, i: 0},
+			2: {a: 0, b: 9, c: 8, d: 0, e: 0, f: 0, g: 0, h: 6, i: 0},
+			3: {a: 8, b: 0, c: 0, d: 0, e: 6, f: 0, g: 0, h: 0, i: 3},
+			4: {a: 4, b: 0, c: 0, d: 8, e: 0, f: 3, g: 0, h: 0, i: 1},
+			5: {a: 7, b: 0, c: 0, d: 0, e: 2, f: 0, g: 0, h: 0, i: 6},
+			6: {a: 0, b: 6, c: 0, d: 0, e: 0, f: 0, g: 2, h: 8, i: 0},
+			7: {a: 0, b: 0, c: 0, d: 4, e: 1, f: 9, g: 0, h: 0, i: 5},
+			8: {a: 0, b: 0, c: 0, d: 0, e: 8, f: 0, g: 0, h: 7, i: 9}
+		};
+		const invalid_numbers = [
+			[5,3,0,0,7,0,0,0,0],
+			[-6,0,0,1,10,5,0,0,0],
+			[0,9,8,0,0,0,0,6,0],
+			[8,0,0,0,-6,0,0,0,3],
+			[4,0,0,8,0,3,0,0,1],
+			[7,0,0,0,2,0,0,0,-6],
+			[0,-6,0,0,0,0,2,8,0],
+			[0,0,0,4,1,10,0,0,5],
+			[0,0,0,0,8,0,0,7,10]
+		];
+		const invalid_size = [
+			[5,3,0,0,7,0,0,0,0],
+			[6,0,0,1,9,5,0,0,0],
+			[0,9,8,0,0,0,0,6,0],
+			[8,0,0,0,6,0,0,0,3],
+			[4,0,0,8,0,3,0,0,1],
+			[7,0,0,0,2,0,0,0,6],
+			[0,6,0,0,0,0,2,8,0],
+			[0,0,0,4,1,9,0,0,5]
+		];
+
+		it('throws an error (invalid variable type)', test_invalid_matrix_throws(invalid_type));
+		it('throws an error (invalid object properties)', test_invalid_matrix_throws(invalid_object));
+		it('throws an error (invalid number in matrix)', test_invalid_matrix_throws(invalid_numbers));
+		it('throws an error (invalid matrix size)', test_invalid_matrix_throws(invalid_size));
+	});
+
+	describe('when invalid number placement is given in matrix', function () {
+		// details_func is a function that returns true for a given details array
+		function test_invalid_placement_throws(matrix, details_func) {
+			return () => {
+				const result = () => {return new Bruteforcer(matrix)};
+				expect(result).to.throw(InvalidSudokuError).with.property("details").which.satisfies(details_func);
+			}
+		}
+
+		const duplicate_row = [
+			[5,3,0,0,7,0,0,0,0],
+			[6,0,0,1,9,5,0,0,0],
+			[0,9,8,0,0,0,0,6,0],
+			[8,0,0,0,6,0,0,0,3],
+			[4,0,0,8,0,3,0,0,1],
+			[7,0,0,0,2,0,0,0,6],
+			[0,6,0,0,0,0,2,8,0],
+			[0,0,0,4,1,9,0,0,5],
+			[9,0,0,0,8,0,0,7,9]
+		]
+		const duplicate_column = [
+			[5,3,0,0,7,0,0,0,0],
+			[6,0,0,1,9,5,0,0,0],
+			[0,9,8,0,0,0,0,6,5],
+			[8,0,0,0,6,0,0,0,3],
+			[4,0,0,8,0,3,0,0,1],
+			[7,0,0,0,2,0,0,0,6],
+			[0,6,0,0,0,0,2,8,0],
+			[0,0,0,4,1,9,0,0,5],
+			[0,0,0,0,8,0,0,7,9]
+		];
+		const duplicate_block = [
+			[5,3,0,0,7,0,0,0,0],
+			[6,0,0,1,9,5,0,0,0],
+			[0,9,8,0,0,0,0,6,0],
+			[8,0,0,0,6,0,0,0,3],
+			[4,0,0,8,0,3,0,0,1],
+			[7,0,0,0,2,0,3,0,6],
+			[0,6,0,0,0,0,2,8,0],
+			[0,0,0,4,1,9,0,0,5],
+			[0,0,0,0,8,0,0,7,9]
+		];
+		const duplicate_multiple = [
+			[5,3,0,0,7,0,0,0,0],
+			[6,0,0,1,9,5,0,0,0],
+			[0,9,8,0,0,0,0,6,5],
+			[8,0,0,0,6,0,0,0,3],
+			[4,0,0,8,0,3,0,0,1],
+			[7,0,0,0,2,0,3,0,6],
+			[0,6,0,0,0,0,2,8,0],
+			[0,0,0,4,1,9,0,0,5],
+			[9,0,0,0,8,0,0,7,9]
+		];
+
+		it('throws an error (duplicate in row)', test_invalid_placement_throws(duplicate_row, function (details) {
+			return (details.length === 1 && details[0].match(/row/i));
+		}));
+		it('throws an error (duplicate in column)', test_invalid_placement_throws(duplicate_column, function (details) {
+			return (details.length === 1 && details[0].match(/column/i));
+		}));
+		it('throws an error (duplicate in block)', test_invalid_placement_throws(duplicate_block, function (details) {
+			return (details.length === 1 && details[0].match(/block/i));
+		}));
+		it('throws an error (multiple duplicates)', test_invalid_placement_throws(duplicate_multiple, function (details) {
+			return (details.length === 3);
+		}));
+	});
 });
